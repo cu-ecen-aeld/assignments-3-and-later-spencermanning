95c9b3c0323d3552751d5717f9fd924188d5a036
Spencer Manning <spencerhmanning@gmail.com>
2023-06-26 20:43:13 -0400

    Continue on accept() fail, don't exit
    Adjust many other things

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -8,6 +8,10 @@
 #include <signal.h>     // for sigaction/SIGINT/SIGTERM
 #include <unistd.h>     // for read()
 #include <errno.h>      // for errno
+#include <arpa/inet.h>  // for inet_ntop()
+// extras
+#include <netinet/in.h>
+#include <fcntl.h>
 
 // const struct {
 //     sa_family_t sa_family;
@@ -25,8 +29,6 @@
 //     struct addrinfo *ai_next;
 // } addrinfo; // servinfo or res
 
-#define MAX_BUF_LEN 20 // TODO: Could it be larger?
-
 int received_exit_signal = 0;
 struct addrinfo *servinfo; // res
 int sockfd;
@@ -39,10 +41,13 @@ void close_all_things() {
 }
 
 static void signal_handler(int signal_num) {
+    /* Logs message to the syslog “Caught signal, exiting” when SIGINT or SIGTERM is received. */
+    syslog(LOG_ERR, "Caught signal, exiting\n");
+    
     if (signal_num == SIGINT) { // ctrl+c
         printf("***We got a SIGINT***\n");
 
-        close_all_things();
+        // close_all_things();
 
         received_exit_signal = 1;
         
@@ -50,26 +55,40 @@ static void signal_handler(int signal_num) {
     else if (signal_num == SIGTERM) {
         printf("***We got a SIGTERM***\n");
 
-        close_all_things();
+        // close_all_things();
 
         received_exit_signal = 1;
     }
 }
 
 int main (int argc, char *argv[]) {
-    // socklen_t socklen;
+    // 5. Modify your program to support a -d argument which runs the aesdsocket application as a daemon.
+    // When in daemon mode the program should fork after ensuring it can bind to port 9000.
+    if (argc == 2) {
+        syslog(LOG_ERR, "Start daemon\n");
+        // TODO: Fill in the daemon here
 
-    // argc = ?
-    // argv[0] = ?
-    // argv[1] = ?
+        if (strcmp(argv[1], "-d") == 0) {
+            syslog(LOG_NOTICE, "About to fork the process");
+            int pid = fork();
+            if (pid == -1) {
+                syslog(LOG_ERR, "Fork failed");
+                exit(1);
+            }
+            else if (pid > 0) { // parent
+                syslog(LOG_NOTICE, "In parent, exiting now");
+                closelog();
+                exit(0);
+            }
+            // Create the new session for the child process.
+            // ie put it in the background as a daemon!
+            setsid();
 
-    // printf("Arguments are %s, %s, %s\n", argv[0], argv[1], argv[2]);
-    // Exit if arguments aren't supplied
-    // if (argc != 3) {
-    //     printf("Incorrect arguments given: argc=%d\n", argc);
-    //     return 1;
-    // }
-
+            // Change working directory to "/"
+            chdir("/"); // Do we need this?
+        }
+    }
+    
     // Open logger
     openlog(NULL,0,LOG_USER);
     syslog(LOG_DEBUG, "Start aesdsocket.c\n");
@@ -84,13 +103,6 @@ int main (int argc, char *argv[]) {
     sigaction(SIGTERM, &new_action, NULL);
     sigaction(SIGINT, &new_action, NULL);
 
-    // b. Opens a stream socket bound to port 9000, failing and returning -1 if any of the socket connection steps fail.
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        syslog(LOG_ERR, "Socket connection failed\n");
-        return -1; 
-    }
-
     // Get the socket address (sockaddr)
     struct addrinfo hints;
     // struct addrinfo *servinfo; // res
@@ -101,46 +113,47 @@ int main (int argc, char *argv[]) {
     
     int getaddrinfoout = getaddrinfo(NULL, "9000", &hints, &servinfo);
     if (getaddrinfoout == -1) {
-        return -1;
+        exit(1);
     }
 
-    // socklen_t addrlen = sizeof(servinfo->ai_addrlen);
+    // b. Opens a stream socket bound to port 9000, failing and returning -1 if any of the socket connection steps fail.
+    sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if (sockfd == -1) {
+        syslog(LOG_ERR, "Socket connection failed\n");
+        exit(1); 
+    }
 
-    // Make the address able to e used again with SO_REUSEADDR
+    // Make the address able to be used again with SO_REUSEADDR
     int myoptval = 1;
     int sockoptout = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &myoptval, sizeof(myoptval));
     if (sockoptout == -1) {
-        return -1;
+        exit(1);
     }
 
     int bindout  = bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
     if (bindout == -1) {
-        return -1;
+        exit(1);
     }
-    
-    // FIXME: Does the daemon creation need to be placed here?
-
-
     // c. Listens for and accepts a connection
-    // socklen_t addrlen = sizeof(addrinfo.ai_addr); // Should it be this way?
     int listenout = listen(sockfd, 20); // 20 from TA
     if (listenout == -1) {
-        return -1;
+        exit(1);
     }
 
-    struct sockaddr_in clientinfo;
+    struct sockaddr_storage clientinfo;
+    // struct sockaddr_in clientinfo;
     socklen_t client_addr_size = sizeof(clientinfo);
 
     // Start with a clean file
     remove("/var/tmp/aesdsocketdata");
 
-    FILE *file;
-    file = fopen("/var/tmp/aesdsocketdata", "w+");
-    if (!file) {
-        syslog(LOG_ERR, "File didn't open\n");
-        printf("File didn't open\n");
-        return -1;
-    }
+    // FILE *file;
+    // file = fopen("/var/tmp/aesdsocketdata", "w+");
+    // if (!file) {
+    //     syslog(LOG_ERR, "File didn't open\n");
+    //     printf("File didn't open\n");
+    //     exit(1);
+    // }
 
     char *line = NULL; // for getline()
 
@@ -149,40 +162,58 @@ int main (int argc, char *argv[]) {
 
         char ipaddr[INET_ADDRSTRLEN]; // IPv4 or IPv6??
 
-        int acceptfd = accept(sockfd, (struct sockaddr*)&clientinfo, &client_addr_size); // TODO: Should servinfo->ai_addr be socklen_t socklen instead?
+        int acceptfd = accept(sockfd, (struct sockaddr*)&clientinfo, &client_addr_size);
         if (acceptfd == -1) {
-            return -1;
+            continue;
         }
 
-        // 5. Modify your program to support a -d argument which runs the aesdsocket application as a daemon.
-        // When in daemon mode the program should fork after ensuring it can bind to port 9000.
-        if (argc == 2) {
-            syslog(LOG_ERR, "Start daemon\n");
-            // TODO: Fill in the daemon here
+        // // 5. Modify your program to support a -d argument which runs the aesdsocket application as a daemon.
+        // // When in daemon mode the program should fork after ensuring it can bind to port 9000.
+        // if (argc == 2) {
+        //     syslog(LOG_ERR, "Start daemon\n");
+        //     // TODO: Fill in the daemon here
 
-            if (strcmp(argv[1], "-d") == 0) {
-                syslog(LOG_NOTICE, "About to fork the process");
-                int pid = fork();
-                if (pid == -1) {
-                    syslog(LOG_ERR, "Fork failed");
-                    return -1;
-                }
-                else if (pid > 0) { // parent
-                    syslog(LOG_NOTICE, "In parent, exiting now");
-                    closelog();
-                    exit(0);
-                }
-                // Create the new session for the child process.
-                // ie put it in the background as a daemon!
-                setsid();
+        //     if (strcmp(argv[1], "-d") == 0) {
+        //         syslog(LOG_NOTICE, "About to fork the process");
+        //         int pid = fork();
+        //         if (pid == -1) {
+        //             syslog(LOG_ERR, "Fork failed");
+        //             exit(1);
+        //         }
+        //         else if (pid > 0) { // parent
+        //             syslog(LOG_NOTICE, "In parent, exiting now");
+        //             closelog();
+        //             exit(0);
+        //         }
+        //         // Create the new session for the child process.
+        //         // ie put it in the background as a daemon!
+        //         setsid();
 
-                // Change working directory to "/"
-                chdir("/"); // Do we need this?
-            }
+        //         // Change working directory to "/"
+        //         chdir("/"); // Do we need this?
+        //     }
+        // }
+
+        // Set up client ip address info
+        if (clientinfo.ss_family == AF_INET) {
+            struct sockaddr_in *addr =  (struct sockaddr_in *)&clientinfo;
+            inet_ntop(AF_INET, addr, ipaddr, sizeof(ipaddr));
         }
-        
+        else if (clientinfo.ss_family == AF_INET6) {
+            struct sockaddr_in6 *addr = (struct sockaddr_in6 *)&clientinfo;
+            inet_ntop(AF_INET, addr, ipaddr, sizeof(ipaddr));
+        }
+
         // d. Logs message to the syslog “Accepted connection from xxx” where XXXX is the IP address of the connected client.
-        syslog(LOG_NOTICE, "Accepted connection from %s\n", servinfo->ai_addr->sa_data);
+        syslog(LOG_NOTICE, "Accepted connection from %s\n", ipaddr);
+
+        FILE *file;
+        file = fopen("/var/tmp/aesdsocketdata", "a+");
+        if (!file) {
+            syslog(LOG_ERR, "File didn't open\n");
+            printf("File didn't open\n");
+            exit(1);
+        }
 
         // e. Receives data over the connection and appends to file /var/tmp/aesdsocketdata, creating this file if it doesn’t exist.
         /*
@@ -206,7 +237,7 @@ int main (int argc, char *argv[]) {
             if (recvbyte == 0 || recvbyte == -1) {
                 syslog(LOG_ERR, "Socket recv() received a %i\n", (int)recvbyte);
                 // syslog(LOG_ERR, "Errno: %d\n", errno);
-                // return -1; // use break; instead??
+                // exit(1); // use break; instead??
             }
             // memcpy(&sockbuf[cnt], &sockbuf1byte, 1);
             // cnt++;
@@ -241,7 +272,7 @@ int main (int argc, char *argv[]) {
         // cnt = 0;
         int fseekerr = fseek(file, 0, SEEK_SET);
         if (fseekerr) {
-            return -1;
+            exit(1);
         }
         // size_t size1 = 1;
         // size_t nmemb = 1;
@@ -278,20 +309,14 @@ int main (int argc, char *argv[]) {
         // } while (ch != EOF);
         // send(acceptfd, ch_all, MAX_BUF_LEN, 0);
 
-
-
         free(line);
+        fclose(file);
         close(acceptfd);
         // TODO: g. Logs message to the syslog “Closed connection from XXX” where XXX is the IP address of the connected client.
         syslog(LOG_NOTICE, "Closed connection from %s\n", ipaddr);
     }
 
     // TODO: i. Gracefully exits when SIGINT or SIGTERM is received, completing any open connection operations, closing any open sockets, and deleting the file /var/tmp/aesdsocketdata.
-    /*
-    Logs message to the syslog “Caught signal, exiting” when SIGINT or SIGTERM is received.
-    */
-    syslog(LOG_ERR, "Caught signal, exiting\n");
-    fclose(file);
     printf("Caught signal, exiting\n");
     remove("/var/tmp/aesdsocketdata");
     shutdown(sockfd,SHUT_RDWR);
