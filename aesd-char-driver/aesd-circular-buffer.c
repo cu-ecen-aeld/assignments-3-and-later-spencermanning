8f0b3dbeac09f04f495a70def5f648b2ee8bc609
Spencer Manning <spencerhmanning@gmail.com>
2024-01-13 21:59:37 -0500

    Fill in both asy7.1 functions

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -32,7 +32,77 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
-//    asdf
+    // int entryptr = NULL;
+    uint8_t index = 0;
+    // uint8_t curr_pos = 0;
+    struct aesd_buffer_entry *entry;
+    int char_offset_signed = char_offset; // to allow it to go negative
+
+    /* 4 possible scenarios to search through
+    A. |out/in             | starting state. out = in. Buffer is empty.
+    B. |  out------->in    | in has advanced but isn't yet behind out.
+    C. |-->in       out----| in has rolled over and is now behind out.
+    D. |------>out/in------| out has caught up to in. out = in again. Buffer is full.
+    */
+    // Scenario A
+    if (buffer->out_offs == buffer->in_offs && !buffer->full) {
+        // do nothing since the buffer has nothing in it.
+    }
+
+    // Scenario B
+    if (buffer->in_offs > buffer->out_offs) {
+        AESD_CIRCULAR_BUFFER_FOREACH(entry, buffer, index) {
+            // skip empty indices
+            if (index < buffer->out_offs || index > buffer->in_offs) {
+                continue;
+            }
+
+            // check current index
+            char_offset_signed -= buffer->entry[index].size;
+            if (char_offset_signed < 0) {
+                // Add back in the subtracted size and return
+                *entry_offset_byte_rtn = char_offset_signed + buffer->entry[index].size;
+                return entry;
+            }
+        }
+    }
+    
+    // Scenario C or Scenario D (same situation as far as looping goes)
+    if ((buffer->out_offs >= buffer->in_offs) || // Scenario C
+        (buffer->out_offs == buffer->in_offs && buffer->full)) { // Scenario D
+        // from out_offs to the end
+        AESD_CIRCULAR_BUFFER_FOREACH(entry, buffer, index) {
+            if (index < buffer->out_offs) {
+                continue;
+            }
+
+            // check current index
+            char_offset_signed -= buffer->entry[index].size;
+            if (char_offset_signed < 0) {
+                // Add back in the subtracted size and return
+                *entry_offset_byte_rtn = char_offset_signed + buffer->entry[index].size;
+                return entry;
+            }
+        }
+
+        // now from the beginning to in_offs
+        index = 0;
+        AESD_CIRCULAR_BUFFER_FOREACH(entry, buffer, index) {
+            // check current index
+            char_offset_signed -= buffer->entry[index].size;
+            if (char_offset_signed < 0) {
+                // Add back in the subtracted size and return
+                *entry_offset_byte_rtn = char_offset_signed + buffer->entry[index].size;
+                return entry;
+            }
+
+            if (index > buffer->in_offs) {
+                // error occurred! should not get here
+                return entry;
+            }
+        }
+    }
+
     return NULL;
 }
 
@@ -48,6 +118,23 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+    // Add the input value to the buffer
+    memcpy(&buffer->entry[buffer->in_offs++], add_entry, sizeof(*add_entry));
+
+    // Advance input offset
+    buffer->in_offs++;
+    buffer->in_offs %= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+    if (buffer->full) {
+        // Advance output offset as well. Previous data is lost.
+        buffer->out_offs++;
+        buffer->out_offs %= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+    else {
+        if (buffer->in_offs == buffer->out_offs) {
+            buffer->full = true;
+        }
+    }
 }
 
 /**
