653408515e7c6860361332da69484e7058299dd9
Spencer Manning <spencerhmanning@gmail.com>
2024-02-20 23:15:31 -0500

    Add helper script

---
+++ aesd-char-driver/module_test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Starting module test script"
+
+echo "---Cleaning"
+make clean
+echo "---Making"
+make
+
+if [ $? -ne 0 ]; then
+    echo "Make failed"
+    exit 1
+fi
+
+echo "---Unloading"
+./aesdchar_unload
+echo "---Loading"
+./aesdchar_load
+
+if [ "$1" = "d" ]; then
+    echo "---Running drivertest.sh"
+    ../assignment-autotest/test/assignment8/drivertest.sh
+fi
